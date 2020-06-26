2db3bc492871bfd7dc4b1450e79c6b8200047498
Alexander Lopez <lopezalexander@google.com>
2020-06-26 22:35:52 +0000

    Add javascript interactivity and finish styling.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,11 @@
+const IMAGE_ARRAY = ['/images/young_pc.jpg', '/images/pico_fogata.JPG','/images/pico_17.jpg','/images/pico_07.JPG','/images/killington.jpg','/images/saona.jpg'];
 
 function getRandomImage() {
-    alert("New Image!");
+    var gallery = document.getElementById('gallery_view');
+    var length = IMAGE_ARRAY.length;
+    var result = Math.floor(Math.random()*length);
+
+    gallery.src = IMAGE_ARRAY[result];
+
+    console.log(result, IMAGE_ARRAY[result]);
 }
