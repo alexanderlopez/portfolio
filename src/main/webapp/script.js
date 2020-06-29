411acf23fa41d0701873aca1b5f7970fc89a456e
Alexander Lopez <alexanderlopez00@gmail.com>
2020-06-29 21:37:29 +0000

    Add fetch functionality from the walkthrough

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,6 @@
-const IMAGE_ARRAY = ['/images/young_pc.jpg', '/images/pico_fogata.JPG','/images/pico_17.jpg','/images/pico_07.JPG','/images/killington.jpg','/images/saona.jpg'];
+const IMAGE_ARRAY = ['/images/young_pc.jpg', '/images/pico_fogata.JPG',
+    '/images/pico_17.jpg','/images/pico_07.JPG','/images/killington.jpg',
+    '/images/saona.jpg'];
 
 function getRandomImage() {
     var gallery = document.getElementById('gallery_view');
@@ -8,4 +10,12 @@ function getRandomImage() {
     gallery.src = IMAGE_ARRAY[result];
 
     console.log(result, IMAGE_ARRAY[result]);
+}
+
+function addServletText() {
+    fetch('/data').then((response) => response.text()).then(response_text => {
+        document.getElementById('fetch_test').innerHTML = response_text;
+        console.log(response_text);
+    });
+    console.log('Done');
 }
