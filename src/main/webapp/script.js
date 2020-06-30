dbc00078ab34d83f93917a389d0670d2a0efa154
Alexander Lopez <alexanderlopez00@gmail.com>
2020-06-30 16:48:34 +0000

    Add comment section display functionality

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -9,3 +9,16 @@ function getRandomImage() {
 
     gallery.src = IMAGE_ARRAY[result];
 }
+
+function getData() {
+    fetch('/data').then(response => response.json()).then(respObj => {
+        var comTable = document.getElementById('com_table');
+        var newRow = comTable.insertRow(-1);
+
+        var nameCell = newRow.insertCell(0);
+        var commentCell = newRow.insertCell(1);
+
+        nameCell.innerHTML = respObj.name;
+        commentCell.innerHTML = respObj.comment;
+    });
+}
