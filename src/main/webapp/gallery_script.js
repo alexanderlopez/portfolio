695718c61f54d6afc92037c46a7de5b94acc521b
Alexander Lopez <alexanderlopez00@gmail.com>
2020-07-08 14:34:58 +0000

    Change name Maps to Location

--- src/main/webapp/gallery_script.js
+++ src/main/webapp/gallery_script.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 18.471095, lng: -69.924035 },
-    zoom: 12
-  });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 18.471095, lng: -69.924035 },
+        zoom: 12
+    });
 }
